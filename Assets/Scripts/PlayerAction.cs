7ade05eb8ec33668c3c62ce3f30bda7038525db6
CHW-Y <83206220+CHW-Y@users.noreply.github.com>
2021-09-05 21:52:16 +0900

    21.09.05 음식 먹는 모션

---
+++ Assets/Scripts/PlayerAction.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    //private void OnCollisionEnter(Collision coll)
+    //{
+    //    if (coll.gameObject.tag == "Food")
+    //    {
+    //        print("음식을 먹는다");
+    //        anim.SetTrigger("Eat");
+    //    }
+    //}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Food")
+        {
+            print("음식을 먹는다");
+            anim.SetTrigger("Eat");
+        }
+    }
+}
