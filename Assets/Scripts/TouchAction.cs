e8c0bcbbbbcd6ea4a56c9480c70140dc900db383
CHW-Y <83206220+CHW-Y@users.noreply.github.com>
2021-09-08 16:43:47 +0900

    21.09.08 상호작용 수정

--- Assets/Scripts/TouchAction.cs
+++ Assets/Scripts/TouchAction.cs
@@ -6,64 +6,68 @@ using UnityEngine.XR.ARSubsystems;
 
 public class TouchAction : MonoBehaviour
 {
-    [SerializeField]
-    GameObject girl;
-    [SerializeField]
     Animator anim;
 
-    public GameObject test1;
-    public GameObject test2;
-    public GameObject test3;
-    public GameObject test4;
-    //ARRaycastManager raycastManager;
-    //List<ARRaycastHit> hits = new List<ARRaycastHit>();
     bool isSelected = false;
 
-    //private void Start()
-    //{
-    //    if (girl == null) girl = GameObject.FindGameObjectWithTag("Girl");
-    //    if (anim == null) anim = girl.GetComponent<Animator>();
-    //    //raycastManager = GetComponent<ARRaycastManager>();
-    //}
+
+
     void Update()
     {
         // 터치가 시작되었다면
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-            if (test1 != null) test1.SetActive(true);
+
             // 터치한 좌표를 레이로 바꾼다.
             Ray ray = Camera.main.ScreenPointToRay(touch.position);
 
-            //if (raycastManager.Raycast(ray, hits))
             if (Physics.Raycast(ray, out RaycastHit hitInfo))
             {
                 // 터치가 시작 시
                 if (touch.phase == TouchPhase.Began)
                 {
-                    if (test2 != null) test2.SetActive(true);
-                    // 선택한 오브젝트의 태그가 Girl이면
-                    if (hitInfo.collider.tag == "Girl")
+                    // 선택한 오브젝트의 태그가 Girl이고, 캐릭터 선택 중이 아니라면
+                    if (hitInfo.collider.tag == "Girl" && !isSelected)
                     {
-                        if (test3 != null) test3.SetActive(true);
+                        //hitInfo.transform.gameObject.TryGetComponent(out anim);
+                        // 애니메이터를 받아온다.
+                        anim = hitInfo.transform.GetComponent<Animator>();
 
-                        hitInfo.transform.gameObject.TryGetComponent(out anim);
-                        
                         // 선택 애니메이션 실행
                         anim.SetTrigger("Select");
 
+                        // 캐릭터 선택 중
                         isSelected = true;
+                    }
+                    // 캐릭터를 선택 중인데 다른 곳을 터치하면
+                    else
+                    {
+                        // 캐릭터 선택 해제
+                        isSelected = false;
                     }
                 }
                 // 터치 후 움직일 시
                 if (touch.phase == TouchPhase.Moved)
                 {
-                    if (hitInfo.collider.tag == "head")
+                    if (hitInfo.collider.tag == "head" && isSelected)
                     {
-                        if (test4 != null) test4.SetActive(true);
+                        //anim = hitInfo.transform.GetComponentInParent<Animator>();
+
                         // 쓰다듬기 상호작용 애니메이션 실행
-                        anim.SetTrigger("Stroke");
+                        anim.SetBool("Stroke", true);
                     }
+                    else
+                    {
+                        // 쓰다듬기 상호작용 애니메이션 종료
+                        anim.SetBool("Stroke", false);
+                    }
+                }
+                // 터치가 끝나면
+                if (touch.phase == TouchPhase.Ended)
+                {
+                    // 쓰다듬기 상호작용 애니메이션 종료
+                    anim.SetBool("Stroke", false);
                 }
             }
         }
