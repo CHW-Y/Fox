ccd7b45ade270581ab504473a04153f330f6c31b
당근이징 <hsp1603@naver.com>
2021-09-12 19:13:15 +0900

    UI 변경 작업 및 옷장 모달 변경 작업 완료

--- Assets/Scripts/ClosetSetActive.cs
+++ Assets/Scripts/ClosetSetActive.cs
@@ -6,19 +6,23 @@ public class ClosetSetActive : MonoBehaviour
 {
     public GameObject closetModal;
     public GameObject closetExit;
+    public GameObject backGround;
 
     void Start()
     {
         closetModal.SetActive(false);
+        backGround.SetActive(false);
     }
 
     public void ClosetClick()
     {
         closetModal.SetActive(true);
+        backGround.SetActive(true);
     }
 
     public void ClosetExitClick()
     {
         closetModal.SetActive(false);
+        backGround.SetActive(false);
     }
 }
