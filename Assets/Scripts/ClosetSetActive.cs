ad58d243677967a05c9aa919a4750ac021544b03
yejin21 <83255075+yejin21@users.noreply.github.com>
2021-09-13 16:44:54 +0900

    아바타 체인지 작업중

--- Assets/Scripts/ClosetSetActive.cs
+++ Assets/Scripts/ClosetSetActive.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class ClosetSetActive : MonoBehaviour
 {
+    // 
+    // dressSlot의 버튼?을 누르면 
+    // costumes의 
+    public GameObject[] costumes;
+    public GameObject[] dressSlot;
+
     public void ClosetClick()
     {
         gameObject.SetActive(true);
